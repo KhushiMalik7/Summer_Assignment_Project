7bcef0394c2991062473cb35419563923d81a864
KhushiMalik7 <khushimalik.sh7@gmail.com>
2026-06-02 11:56:13 +0530

    "Day 2 Assignment"

---
+++ Day2.cpp
@@ -0,0 +1,66 @@
+//#include<iostream>
+//using namespace std;
+
+// =========== DAY 2 ASSIGNMENT ===========
+
+//Q.5- SUM OF DIGITS OF A NUMBER
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int n, sum = 0;
+//	cout << "Enter n : ";
+//	cin >> n;
+//	while (n > 0) {
+//		sum += n % 10;
+//		n /= 10;
+//	}
+//	cout << "Sum of digits is " << sum << endl;
+//	return 0;}
+
+//Q.6- REVERSE A NUMBER
+
+//
+
+//Q.7- PRODUCT OF DIGITS;
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int n, product = 1;
+//	cout << "Enter n : ";
+//	cin >> n;
+//	while (n > 0) {
+//		product *= n % 10; //product formula;
+//		n /= 10;
+//	}
+//	cout << "Product of digits is " << product << endl;
+//	return 0;
+//}
+
+//Q.8- CHECK IF A NUMBER IS PALINDROME OR NOT
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int n, original, reverse = 0;
+//	cout << "Enter n : ";
+//	cin >> n;
+//	original = n; //store the original number
+//	while (n > 0) {
+//		reverse = reverse * 10 + n % 10; //reverse formula
+//		n /= 10;
+//	}
+//	if (original == reverse) {
+//		cout << "The number is a palindrome." << endl;
+//	}
+//	else {
+//		cout << "The number is not a palindrome." << endl;
+//	}
+//	return 0;
+//}
+
+//int main() {
+//	cout << " ========== DAY 2 ASSIGNMENT ===========" << endl;
+//	return 0;
+//}
