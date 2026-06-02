c290ddd009dbe71081725adb14cc79236ac037de
KhushiMalik7 <khushimalik.sh7@gmail.com>
2026-06-02 10:49:24 +0530

    Add project files.

---
+++ Day1.cpp
@@ -0,0 +1,71 @@
+// #include<iostream>
+// using namespace std ;
+
+// =========== DAY 1 ASSIGNMENT ==========
+
+//Q.1- sum of first n natural numbers
+// 
+//#include<iostream>
+//using namespace std ;
+//int main() {
+//	int n, sum = 0;
+//	cout << "Enter n : ";
+//	cin >> n;
+//	for (int i = 1; i <= n; i++) {
+//		sum += i;
+//	}
+//	cout << "sum=" << sum << endl;
+//	return 0;
+//}
+
+
+//Q.2- MULTIPLICATION TABLE
+
+//#include<iostream>
+//using namespace std ;
+//int main() {
+//	int n;
+//	cout << "Enter n : ";
+//	cin >> n;
+//	for (int i = 1; i <= 10; i++) {
+//		cout << n << " * " << i << " = " << n * i << endl;
+//	}return 0;
+//}
+
+ // Q.3- FACTORIAL OF A NUMBER
+  
+//#include<iostream>
+//using namespace std ;
+//int main() {
+//	int n, fact = 1;
+//	cout << "Enter n : ";
+//	cin >> n;
+//	for (int i = 1; i <= n; i++) {
+//		fact *= i;
+//	}
+//	cout << "Factorial of " << n << " is " << fact << endl;
+//	return 0;
+//}
+
+// Q.4-COUNT DIGITS
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int n, count = 0;
+//	cout << "Enter n : ";
+//	cin >> n;
+//	while (n > 0) {
+//		n /= 10;
+//		count++;
+//	}
+//	cout << "Number of digits is " << count << endl;
+//	return 0;
+//}
+
+//int main() {
+
+//	cout << "========== DAY 1 ASSIGNMENT ========== " << endl;
+
+//	return 0;
+//}
