fa546a402afa17eb232dc0a1dcbd6ab1d160a3e6
KhushiMalik7 <khushimalik.sh7@gmail.com>
2026-06-03 09:48:07 +0530

    "Day 3 Assignment"

---
+++ Day 3 .cpp
@@ -0,0 +1,72 @@
+//#include<iostream>
+//using namespace std;
+
+// ================= DAY 3 ASSIGNMENT ==================
+
+// Q.9 CHECK WHETHER A NUMBER IS PRIME OR NOT
+//
+//#include<iostream>
+//using namespace std;
+// int main (){
+// int n ;
+// cout << " enter n :";
+// cin >> n ;
+// for (int i = 1 ; i <= n ; i++)
+// if (n % 2 == 0 ){
+// cout << " prime number " << endl;
+// }
+// else { 
+// cour << " not prime number << endl ;
+// } 
+// return 0;
+// }
+
+
+//Q.10 PRINT PRIME NUMBER IN RANGE 
+
+//#include<iostream>
+//using namespace std;
+//int main (){
+//	int a,b; // a is start of range and b is end of range;
+//	cout << " enter start of range :";
+//	cin >> a; // start of range ;
+//	cout << " enter end of range :";
+//	cin >> b; // end of range ;
+//	
+//	cout << " prime number between " << a << " and " << b << " are :" << endl;
+//	 
+//	// loop through each number in range ;
+//	for (int i = a; i <= b; i++) {
+//		 // skip number les than or equal to 1( 0 and 1 are not prime )
+//		
+//		if (i <= 1) {
+//			continue;
+//		}
+//		bool flag = true; // flag true mean is prime ;
+//		for (int j = 2; j * j <= i; j++) {
+//			if (i % j == 0) {
+//				flag = false; // found a factor , so its not prime number 
+//				break; // stop to check further factors;
+//			}
+//		}
+//		// if the number is prime , print it ;
+//		if (flag) {
+//			cout << i << " ";
+//		}
+//	 }
+//	cout << endl;
+//	return 0;
+//}
+
+
+ 
+// Q.12 FIND THE LCM OF TWO NUMBERS
+
+//
+  
+//int main() {
+//	 
+//	cout << " =================== DAY 3 ASSIGNMENT ==================" << endl;
+//
+//	return 0;
+//}
