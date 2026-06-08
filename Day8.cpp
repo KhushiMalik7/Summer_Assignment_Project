6d4e93fe582b667318351fd6b074aa079f0672ac
KhushiMalik7 <khushimalik.sh7@gmail.com>
2026-06-08 21:25:08 +0530

    " Day 7 Assignment "

---
+++ Day8.cpp
@@ -0,0 +1,92 @@
+//#include<iostream>
+//using namespace std;
+
+
+//==================== DAY 8 ASSIGNMENT ======================
+
+//Q.29- PRINT HALIF PYRAMID PTTERN 
+
+//#include<iostream>
+//using namespace std; 
+//int main() {
+//	int n; 
+//	cout << " enter the number of rows :";
+//	cin >> n;
+//
+//	for (int i = 1; i <= n; i++) {
+//		for (int j = 1; j <= i; j++) {
+//			cout << " * ";
+//		}
+//		cout << endl; 
+//	}
+//	return 0;
+//}
+
+
+//Q.30-	PRINT NUMBER TRIANGLE
+
+//#include<iostream>
+//using namespace std; 
+//
+//int main() {
+//
+//	int n;
+//	cout << " enter the number of rows:";
+//	cin >> n; 
+//
+//	for (int i = 1; i <= n; i++) {
+//		for (int j = 1; j <= i; j++) {
+//			cout << j;
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+
+//Q.31- PRINT CHARACTER TRIANGLE 
+
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int n;
+//	cout << " enter the number of rows:";
+//	cin >> n;
+//
+//	for (int i = 1; i <= n; i++) {
+//		for (int j = 0; j < i; j++) {
+//			cout << char('A' + j);
+//		}
+//	cout << endl;
+//	}
+//	return 0;
+//}
+
+
+//Q.32- REPEATED NUMBER PATTERN 
+
+//#include<iostream>
+//using namespace std; 
+//
+//int main() {
+//
+//	int n; 
+//	cout << " enter  number of rows :";
+//	cin >> n; 
+//
+//	for (int i = 1; i <= n; i++) {
+//		for (int j = 1; j <= i; j++) {
+//			cout << i;
+//		}
+//		cout << endl; 
+//	}
+//	return 0;
+//}
+
+//int main() {
+//
+//	cout << " =================== DAY 8 ASSIGNMENT ================== :" << endl;
+//	return 0;
+//}
