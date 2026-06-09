ee5ac8e6ab8a7d393411d44878485495e1b14ed6
KhushiMalik7 <khushimalik.sh7@gmail.com>
2026-06-09 10:27:43 +0530

    " Day 8 Assignment "

---
+++ Day9.cpp
@@ -0,0 +1,90 @@
+//#include<iostream>
+//using namespace std;
+
+
+// ========================== DAY 9 ASSIGNMENT ===========================
+
+
+//Q.33- REVERSE STAR PATTERN
+
+
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	for (int i = 5; i >= 1; i--) {
+//		for (int j = 1; j <= i; j++) {
+//			cout << " * ";
+//		}
+//		cout << endl; 
+//}
+//	return 0;
+//	}
+
+
+//Q.34- REVERSE NUMBER TRIANGLE
+
+//#include<iostream>
+//using namespace std; 
+//
+//int main() {
+//	for (int i = 5; i >= 1; i--) {
+//		for (int j = 1; j <= i; j++) {
+//			cout << j; 
+//		}
+//		cout << endl; 
+//	}
+//	return 0;
+//}
+
+
+//Q.35- REPEATED CHARACTER PATTERN
+
+//#include<iostream>
+//using namespace std; 
+//
+//int main() {
+//
+//	int n; 
+//	cout << " enter n  :";
+//	cin >> n;
+//	for(int i = 1 ; i <= n ; i++)
+//	 {
+//		for (int j = 1; j <= i;j++){
+//			cout << char(64 + i);
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+
+//Q.36- HOLLOW SQUARE PATTERN
+
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	int n;
+//	cout << " enter n :";
+//	cin >> n;
+//
+//	for (int i = 1; i <= n; i++) {
+//		for (int j = 1; j <= n; j++) {
+//			if (i == 1 || i == n || j == 1 || j == n)  // hollow sphere ;
+//				cout << " * ";
+//			else
+//				cout << "   ";
+//
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+
+//int main() {
+//
+//	cout << " ==================== DAY 9 ASSIGNMENT =====================";
+//	return 0;
+// }
