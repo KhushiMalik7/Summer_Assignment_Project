6eed12aab27909bffe2c5f01adeb36e08f33e395
KhushiMalik7 <khushimalik.sh7@gmail.com>
2026-06-10 21:20:34 +0530

    " Day 10 Assignment "

---
+++ Day10.cpp
@@ -0,0 +1,89 @@
+//#include<iostream>
+//using namespace std; 
+
+
+// ====================== DAY 10 ASSIGNMENT ====================
+
+//Q.37- STAR PYRAMID TRIANGLE
+
+
+//#include<iostream>
+//using namespace std;
+// 
+//int main() {
+//	int rows = 5; 
+//
+//	for (int i = 1; i <= rows; i++) {
+//		for (int j = 1; j <= rows - i; j++)
+//			cout << " ";
+//		for (int k = 1; k <= (2 * i - 1); k++)
+//			cout << "*";
+//
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+
+//Q.38- REVERSE PYRAMID 
+
+//#include<iostream>
+//using namespace std; 
+//
+//int main() {
+//	int rows = 5;
+//	for (int i = rows; i >= 1; i--) {
+//		for (int j = 1; j <= rows - i; j++)
+//			cout << " ";
+//		for (int k = 1; k <= (2 * i - 1); k++)
+//			cout << "*";
+//		
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+
+//Q.39- NUMBER PYRAMID
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int n; 
+//	cout << "enter the side of square :";
+//	cin >> n;
+//	for (int i = 1; i <= n; i++) { //row = n;
+//		for (int j = 1; j <=  i; j++) { //columns = n ;
+//			cout <<  j << " ";
+//		}
+//		cout << endl;
+//	
+//	}
+//	                                                                                                                                                                                                                                                                                                                             
+//}
+
+
+//Q.40- CHARACTER PYRAMID
+
+// #include<iostream>
+//using namespace std;
+//int main() {
+//	int n; 
+//	cout << "enter the side of square :";
+//	cin >> n;
+//	for (int i = 1; i <= n; i++) { //row = n;
+//		for (int j = 1; j <= i; j++) { //columns = n ;
+//			cout << ( char )(j + 64)  << " ";
+//		}
+//		cout << endl;
+//	
+//	}
+//	                                                                                                                                                                                                                                                                                                                             
+//}
+
+
+//int main() {
+//
+//	cout << " ========================= DAY 10 ASSIGNMENT =====================" << endl;
+//	return 0;
+//}
