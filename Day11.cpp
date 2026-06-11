da16778ff7ae05720a9ed971f8217520df639f73
KhushiMalik7 <khushimalik.sh7@gmail.com>
2026-06-11 09:17:28 +0530

    " Day 10 Assignment "

---
+++ Day11.cpp
@@ -0,0 +1,119 @@
+//#include<iostream>
+//using namespace std; 
+
+
+// ========================== DAY 11 ASSIGNMENT ==========================
+
+
+//Q.41- WRITE A FUNCTION TO FIND THE SUM OF TWO NUMBERS
+
+//#include<iostream>
+//using namespace std;
+//
+//int sum( int a , int b ) {
+//	return a + b;
+// }
+//
+//int main() {
+//	int x, y; 
+//	cout << " enter two numbers :";
+//	cin >> x >> y;
+//
+//	cout << " sum =" << sum(x, y);
+//
+//	return 0;
+//}
+
+
+
+//Q.42- WRIE A FUNCTION TO FIND MAXIMUM  OF TWO NUMBERS
+
+
+
+//#include<iostream>
+//using namespace std; 
+// 
+//int maximum(int a, int b)
+//{
+//	if (a > b)
+//		return a;
+//	else
+//		return b; 
+// }
+//
+//int main() {
+//	int x, y; 
+//
+//	cout << " enter two numbers : ";
+//	cin >> x >> y;
+//
+//	cout << " Maximum = " << maximum(x, y);
+//
+//	return 0;
+//}
+
+
+
+ //Q.43- WRITE A FUNCTION TO CHECK PRIME NUMBER 
+
+
+
+//#include<iostream>
+//using namespace std; 
+//
+//void checkprime(int n) {
+//	int count = 0; 
+//
+//	for (int i = 1; i <= n; i++) {
+//		if (n % i == 0)
+//			count++;
+//	}
+//	if (count == 2)
+//		cout << " Prime Number :";
+//	else
+//		cout << " Not Prime Number :";
+//}
+//
+//int main() {
+//	int n;
+//	cout << " enter n :";
+//	cin >> n; 
+//
+//	checkprime(n);
+//
+//	return 0;
+//}
+
+
+
+//Q.44- WRITE A FUNCTION TO FIND FACTORIAL 
+
+
+//#include<iostream>
+//using namespace std; 
+//
+//int factorial(int n) {
+//	int fact = 1; 
+//
+//	for (int i = 1; i <= n; i++) {
+//		fact = fact * i; 
+//	}
+//	return fact; 
+// }
+//
+//int main() {
+//
+//	int n; 
+//	cout << " enter a number :";
+//	cin >> n; 
+//	
+//	cout << " Factorial =" << factorial(n);
+//	return 0;
+//}
+ 
+
+//int main() {
+//
+//	cout << " ============================ DAY 11 ASSIGNMENT ==========================" << endl;
+//	return 0;
+//}
