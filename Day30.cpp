68d08ab5637d55a4d397b2ba7ff98d5b43c4e4ca
KhushiMalik7 <khushimalik.sh7@gmail.com>
2026-07-01 11:52:32 +0530

    $

---
+++ Day30.cpp
@@ -0,0 +1,147 @@
+//#include<iostream>
+//using namespace std; 
+
+
+// ====================================================== DAY 30 ==================================================
+
+
+//Q.117- STUDENT RECORD SYSTEM USING ARRAY AND STRING 
+
+
+///*#include<iostream>
+//using namespace std; 
+//
+//int main() {
+//	struct Student {
+//		string name;
+//		int age;
+//		string grade;
+//	};
+//	const int SIZE = 5;
+//	Student students[SIZE];
+//	int choice;
+//	do {
+//		cout << "\nStudent Record System" << endl;
+//		cout << "1. Add Student" << endl;
+//		cout << "2. View Students" << endl;
+//		cout << "3. Exit" << endl;
+//		cout << "Enter your choice: ";
+//		cin >> choice;
+//		switch (choice) {
+//		case 1:
+//			for (int i = 0; i < SIZE; i++) {
+//				cout << "Enter student name: ";
+//				cin >> students[i].name;
+//				cout << "Enter student age: ";
+//				cin >> students[i].age;
+//				cout << "Enter student grade: ";
+//				cin >> students[i].grade;
+//			}
+//			break;
+//		case 2:
+//			cout << "\nList of Students:" << endl;
+//			for (int i = 0; i < SIZE; i++) {
+//				cout << "Name: " << students[i].name
+//					<< ", Age: " << students[i].age
+//					<< ", Grade: " << students[i].grade*/ << endl;
+//			/*}
+//			break;
+//		case 3:
+//			cout << "Exiting the system. Goodbye!" << endl;
+//			break;
+//		default:
+//			cout << "Invalid choice! Please try again." << endl;
+//		}
+//	} while (choice != 3);
+//	return 0;
+//}*/
+
+
+//Q.118- MINI EMPOLYEE MANAGEMENT SYSTEM
+
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	int id; 
+//	string name; 
+//	float salary;
+//
+//	cout << " enter the id :";
+//	cin >> id; 
+//
+//	cout << " enter the name :";
+//	cin >> name;
+//
+//	cout << " enter the salary :";
+//	cin >> salary;
+//
+//	cout << "\nempolyee  details:";
+//	cout << "\n id :" << id;
+//	cout << "\n name :" << name;
+//	cout << "\n salary :" << salary;
+//
+//	return 0;
+//
+//
+//}
+
+
+//Q.119-  MINI LIBRARY MANAGEMENT SYSTEM
+
+
+//#include<iostream>
+//using namespace std; 
+//
+//int main() {
+//	int choice; 
+//	cout << " 1. add book " << endl;
+//	cout << " 2. issue book " << endl;
+//	cout << " 3. return book " << endl;
+//	cin >> choice; 
+//	if (choice == 1) {
+//		cout << " add book " << endl;
+//	}
+//	else if (choice == 2) {
+//		cout << " issue book " << endl;
+//	}
+//	else if (choice == 3) {
+//		cout << " return book " << endl;
+//	}
+//	else {
+//		cout << " invalid choice " << endl;
+//	}
+//	return 0;
+//}
+
+
+//Q.120- MINI PROJECT USING ARRAY , STRING AND FUNCTION 
+
+//#include<iostream>
+//using namespace std; 
+//
+//int main() {
+//	int choice;
+//	cout << " 1. add student " << endl;
+//	cout << " 2. view student " << endl;
+//	cout << " 3. exit " << endl;
+//	cin >> choice;
+//	if (choice == 1) {
+//		cout << " add student " << endl;
+//	}
+//	else if (choice == 2) {
+//		cout << " view student " << endl;
+//	}
+//	else if (choice == 3) {
+//		cout << " exit " << endl;
+//	}
+//	else {
+//		cout << " invalid choice " << endl;
+//	}
+//	return 0;}
+
+
+//int main(){
+// 
+// cout << " ====================================== DAY 30 ============================================:";
+// return 0;}
