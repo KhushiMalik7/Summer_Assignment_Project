0a1df276d66d360e44eff34041c6f85735b5ce21
KhushiMalik7 <khushimalik.sh7@gmail.com>
2026-07-01 11:09:40 +0530

    m

--- Day28.cpp
+++ Day28.cpp
@@ -5,16 +5,105 @@
 // ========================================================== DAY 28 ==========================================================
 
 
-//Q.108- LIBRARY MANAGEMENT SYSTEM
+//Q.109- LIBRARY MANAGEMENT SYSTEM
 
 
-#include<iostream>
-#include<vector>
-#include<string>
-using namespace std;
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	int  choice;
+//
+//	cout << " 1. add book " << endl;
+//	cout << " 2. issue book " << endl;
+//	cout << " 3. return book " << endl;
+//
+//	cin >> choice;
+//
+//	if (choice == 1) {
+//		cout << " add book " << endl;
+//	}
+//	else if (choice == 2) {
+//		cout << " issue book " << endl;
+//	}
+//	else if (choice == 3) {
+//		cout << " return book " << endl;
+//	}
+//	else {
+//		cout << " invalid choice " << endl;
+//	}
+//	return 0;
+//}
+//
+//
+//	
 
-class book {
-public:
-	string title;
-	
-};
+
+//Q.110- BANK ACCOUNT SYSTEM 
+
+
+//#include<iostream>
+//using namespace std; 
+//
+//int main() {
+//
+//	float balance = 1000;
+//	float depositamount; 
+//
+//	cout << " enter the deposite amount :";
+//	cin >> depositamount;
+//
+//	balance = balance + depositamount;
+//
+//	cout << " your new balance is :" << balance << endl;
+//
+//	return 0;
+//}
+
+
+
+//Q.111-  TICKET BOOKING SYSTEM
+
+
+//#include<iostream>
+//using namespace std; 
+//
+//int main() {
+//	int ticketsavailable = 100;
+//	int ticketsbooked;
+//	cout << " enter the number of tickets you want to book :";
+//	cin >> ticketsbooked;
+//	if (ticketsbooked <= ticketsavailable) {
+//		ticketsavailable = ticketsavailable - ticketsbooked;
+//		cout << " your tickets are booked successfully " << endl;
+//		cout << " remaining tickets are :" << ticketsavailable << endl;
+//	}
+//	else {
+//		cout << " sorry, not enough tickets available " << endl;
+//	}
+//	return 0;
+//}
+
+
+//Q.112- CONTACT MANAGEMENT SYSTEM
+
+//#include<iostream>
+//using namespace std; 
+//
+//int main() {
+//	string name;
+//	string phone;
+//	cout << " enter your name :";
+//	cin >> name;
+//	cout << " enter your phone number :";
+//	cin >> phone;
+//	cout << " your contact is saved successfully " << endl;
+//	cout << " name :" << name << endl;
+//	cout << " phone number :" << phone << endl;
+//	return 0;
+//}
+
+
+//int main(){
+// cout << " ================================================ DAY 28 ====================================================== :";
+// return 0;}
